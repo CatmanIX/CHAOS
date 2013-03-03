43afe2ce35bb1a19decd5929f10d73d94dc994e5
The_Catman <catmanix@gmail.com>
2013-03-03 03:14:26 -0500

    working on combiners for socii

--- enhance.bat
+++ enhance.bat
@@ -1,6 +1,14 @@
 type css\webkit.css | cleancss -e -b -o css\webkit.css
 type css\mozilla.css | cleancss -e -b -o css\mozilla.css
 type css\opera.css | cleancss -e -b -o css\opera.css
+type css\combiners-nsfw.css | cleancss -e -b -o css\combiners-nsfw.css
+type css\combiners-sfw.css | cleancss -e -b -o css\combiners-sfw.css
+
 type css\webkit.css | enhancecss -o css\webkit.css
 type css\mozilla.css | enhancecss -o css\mozilla.css
 type css\opera.css | enhancecss -o css\opera.css
+type css\combiners-nsfw.css | enhancecss -o css\combiners-nsfw.css
+type css\combiners-sfw.css | enhancecss -o css\combiners-sfw.css
+
+csstidy css\combiners-nsfw.css --template=high css\combiners-nsfw.css
+csstidy css\combiners-sfw.css --template=high css\combiners-sfw.css
